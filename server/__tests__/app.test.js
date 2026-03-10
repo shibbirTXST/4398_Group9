306f9d2b6e90775c9e18108a5543a9e4e180d6fe
Ryan Giberson <rsg63@txstate.edu>
2026-03-10 11:01:03 -0500

    feature/account-deletion: Added test for account deletion

--- server/__tests__/app.test.js
+++ server/__tests__/app.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const app = require('../app');
+const admin = require('firebase-admin');
 
 //test cases
 //simple test case for root endpoint
@@ -9,5 +10,29 @@ describe('API root', () => {
 
     expect(res.status).toBe(200);
     expect(res.body).toEqual({ message: 'Welcome to the Habit Tracker API' });
+  });
+});
+
+jest.mock('firebase-admin', () => ({
+  auth: jest.fn().mockReturnThis(),
+  deleteUser: jest.fn(),
+}));
+
+describe('DELETE /api/user/:uid', () => {
+  const mockUid = 'firebase-user-123';
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('Should delete user from Firebase', async () => {
+    admin.auth().deleteUser.mockResolvedValueOnce();
+
+    const response = await request(app)
+      .delete(`/api/user/${mockUid}`)
+
+    expect(response.status).toBe(204);
+
+    expect(admin.auth().deleteUser).toHaveBeenCalledWith(mockUid);
   });
 });
