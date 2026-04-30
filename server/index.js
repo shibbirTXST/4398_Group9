664ce998bbf165c8f050f51b019f465a57a44a78
Ryan Giberson <rsg63@txstate.edu>
2026-04-29 21:05:18 -0500

    feature/streak-shields: Modified completeHabit for streak shields

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 import 'dotenv/config';
 import app from './app.js';
 import { startReminderCronJob } from './jobs/reminder.js';
-import { resetHabitsCronJob } from './jobs/habitStreaks.js';
+import { streakCronJob } from './jobs/habitStreaks.js';
 
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   startReminderCronJob();
-  resetHabitsCronJob();
+  streakCronJob();
   console.log('Background cron jobs initialized.');
 });
